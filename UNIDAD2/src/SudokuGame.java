a170f4f00516b60aacaf14c9988bd3e37237edb6
mel091 <159938702+mel091@users.noreply.github.com>
2024-04-04 15:59:50 -0700

    unidad 3 ejercicio 19
    19.- Entrega de rompecabezas númerico 
    
    Al iniciar los números deben estar al azar 
    Al presionar un botón que no tiene espacio aun lado no debe pasar nada
    Al presionar un botón que tiene espacio en blanco aun lado debe intercambiar su valor 
    Cuando gane el juego debe salir una alerta avisando que el juego ha sido completado 
    Si se presiona reiniciar debe revolver los números de nuevo 
    Implementar botón de pausa 
    Contar el tiempo una vez que el juego inicia, se debe detener el tiempo durante la pausa

--- UNIDAD2/src/SudokuGame.java
+++ UNIDAD2/src/SudokuGame.java
@@ -1,160 +1,285 @@
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
+import java.awt.Point;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.Timer;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 public class SudokuGame extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JLabel tiempo;
+    
+    ArrayList<Integer> numbers = new ArrayList<>();
+    private List<JButton> buttons = new ArrayList<>();
+    
+    public boolean ordenado = true;
+    public Timer time;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) 
-	{
-		EventQueue.invokeLater(new Runnable() 
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                	
+                    SudokuGame frame = new SudokuGame();
+                    
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public SudokuGame() 
+    {
+        this.setTitle("Puzzle numbers");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 445, 480);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        
+        setContentPane(contentPane);
+        contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+        
+        JPanel panel = new JPanel();
+        contentPane.add(panel);
+        panel.setLayout(null);
+        panel.setBackground(new Color(72, 61, 139));
+        
+        tiempo = new JLabel("Tiempo: ");
+        tiempo.setBounds(160, 0, 200, 20);
+        tiempo.setForeground(Color.white);
+        //tiempo.setOpaque(true);
+        panel.add(tiempo);
+        
+        
+        reiniciar();
+
+        int[] buttonPosX = {20, 115, 210, 305, 20, 115, 210, 305, 20, 115, 210, 305, 20, 115, 210, 305};
+        int[] buttonPosY = {20, 20, 20, 20, 100, 100, 100, 100, 180, 180, 180, 180, 260, 260, 260, 260};
+        
+        for (int i = 0; i < 16; i++) //crea botones en las coordenadas asignadas
+        {
+            JButton button ;
+            
+            if(numbers.get(i) != 16)
+            {
+            	button = new JButton(numbers.get(i).toString());
+            }
+            else
+            {
+            	button = new JButton("");
+            }
+            
+            button.setFont(new Font("Tahoma", Font.BOLD, 16));
+            button.setBounds(buttonPosX[i], buttonPosY[i], 95, 80);
+            button.setBackground(Color.white);;
+            button.addActionListener(new ActionListener() 
+            {
+                public void actionPerformed(ActionEvent e) {
+                }
+            });
+            panel.add(button);
+            buttons.add(button);
+        }
+        
+        JButton btnPause = new JButton("Pausa");
+        btnPause.setBackground(Color.white);
+        btnPause.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+            	if(time.isRunning())
+            	{
+            		time.stop();
+            		
+            	}
+            	else
+            	{
+            		time.start();
+            	}
+                 
+            }
+        });
+        btnPause.setFont(new Font("Tahoma", Font.BOLD, 16));
+        btnPause.setBounds(230, 350, 140, 70);
+        panel.add(btnPause);
+        
+        JButton btnReinicio = new JButton("Reiniciar");
+        btnReinicio.setBackground(Color.white);
+        btnReinicio.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            	reiniciar();
+
+                for (Component component : panel.getComponents()) 
+                {
+                    if (component instanceof JButton) 
+                    {
+                        JButton button = (JButton) component;
+                        if (button != btnReinicio && button != btnPause) 
+                        {
+                            int index = panel.getComponentZOrder(button);
+                            if (index < numbers.size()) 
+                            {
+                                if (numbers.get(index) != 16) 
+                                {
+                                    button.setText(numbers.get(index).toString());
+                                } 
+                                else 
+                                {
+                                    button.setText("");
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        });
+        
+        btnReinicio.setFont(new Font("Tahoma", Font.BOLD, 16));
+        btnReinicio.setBounds(50, 350, 140, 70);
+        panel.add(btnReinicio);
+        
+        
+        
+        ActionListener buttonActionLis = new ActionListener()
 		{
-			public void run() 
+			@Override
+			public void actionPerformed(ActionEvent e) 
 			{
-				try 
+				JButton btnOriginal = (JButton) e.getSource(); //saber q botón es clickeado
+				JButton btnEmpty = null;
+				
+				for(Component component : panel.getComponents())
 				{
-					SudokuGame frame = new SudokuGame();
-					frame.setVisible(true);
-				} catch (Exception e) 
+					if(component instanceof JButton)
+					{
+						JButton btn = (JButton) component;
+						if(btn.getText().equals(""))
+						{
+							btnEmpty = btn;
+							break;
+						}
+					}
+				}
+				
+				int btnOriginalX = btnOriginal.getBounds().x;
+				int btnOriginalY = btnOriginal.getBounds().y;
+				int emptyBtnX = btnEmpty.getBounds().x;
+				int emptyBtnY = btnEmpty.getBounds().y;
+				
+				//distancia horizontal entre el original y el vacio, comprobando que esta a su lado y NO en la misma columna y si está en la misma fila
+				if((Math.abs(btnOriginalX - emptyBtnX) == 95 && btnOriginalY == emptyBtnY) || 
+				   (Math.abs(btnOriginalY - emptyBtnY) == 80 && btnOriginalX == emptyBtnX))
+					//distancia vertical entre original y vacio, comprobando q esta arriba o abajo y NO en la misma fila, y si está en la misma columna
 				{
-					e.printStackTrace();
+					//almaacena temporalmente la ubicacion de un boton clickeado antes de realizar intercambio
+					Point tempLocation = btnOriginal.getLocation();
+					btnOriginal.setLocation(btnEmpty.getLocation());
+					btnEmpty.setLocation(tempLocation);
+					
+					revalidate();
+					repaint();
+					
+					 if(checkWin()) 
+					 {
+	                        JOptionPane.showMessageDialog(null, "¡Felicidades! ¡Has ganado!");
+	                    }
+					 
+					
+					
+				}
+				
+			}
+	
+		};
+		
+		//MOVIMIENTO NO BORRAR
+		for(Component component : panel.getComponents())
+		{
+			{
+				if(component instanceof JButton)
+				{
+					JButton btn = (JButton) component;
+					btn.addActionListener(buttonActionLis);
 				}
 			}
-		});
-	}
+		}
+		
+		
+		
+	//cuenta segundos de ejecución
+	time = new Timer(1000, new ActionListener() 
+	{
+		private long startTime = System.currentTimeMillis();
+		
+		public void actionPerformed(ActionEvent e)
+		{
+			long elapsedTime = System.currentTimeMillis() - startTime;
+            long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime);
+            tiempo.setText("Tiempo: " + seconds + " segundos");
+		}
+		
+		
+	});	
+	time.start(); //////////////////////////////////////////////////
+		
+       
+    } //no borrar
+    
+    public boolean checkWin() 
+    {
+        int lastIndex = buttons.size() - 1;
+        for (int i = 0; i < lastIndex; i++) 
+        {
+            JButton current = buttons.get(i);
+            JButton next = buttons.get(i + 1);
+            
+            // Verifica si los botones están en orden ascendente y el último botón está vacío
+            if (!current.getText().equals(Integer.toString(i + 1)) || (i == lastIndex - 1 && !next.getText().equals(""))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public SudokuGame() {
-		this.setTitle("Puzzle numbers");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 445, 480);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel);
-		panel.setLayout(null);
-		panel.setBackground(new Color(72, 61, 139));
-		
-		JButton btn1 = new JButton("1");
-		btn1.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btn1.setBounds(20, 20, 95, 80);
-		panel.add(btn1);
-		
-		JButton btn2 = new JButton("2");
-		btn2.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn2.setBounds(115, 20, 95, 80);
-		panel.add(btn2);
-		
-		JButton btn3 = new JButton("3");
-		btn3.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn3.setBounds(210, 20, 95, 80);
-		panel.add(btn3);
-		
-		JButton btn4 = new JButton("4");
-		btn4.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn4.setBounds(305, 20, 95, 80);
-		panel.add(btn4);
-		
-		JButton btn5 = new JButton("5");
-		btn5.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn5.setBounds(20, 100, 95, 80);
-		panel.add(btn5);
-		
-		JButton btn6 = new JButton("6");
-		btn6.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn6.setBounds(115, 100, 95, 80);
-		panel.add(btn6);
-		
-		JButton btn7 = new JButton("7");
-		btn7.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn7.setBounds(210, 100, 95, 80);
-		panel.add(btn7);
-		
-		JButton btn8 = new JButton("8");
-		btn8.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn8.setBounds(305, 100, 95, 80);
-		panel.add(btn8);
-		
-		JButton btn9 = new JButton("9");
-		btn9.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn9.setBounds(20, 180, 95, 80);
-		panel.add(btn9);
-		
-		JButton btn10 = new JButton("10");
-		btn10.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn10.setBounds(115, 180, 95, 80);
-		panel.add(btn10);
-		
-		JButton btn11 = new JButton("11");
-		btn11.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn11.setBounds(210, 180, 95, 80);
-		panel.add(btn11);
-		
-		JButton btn12 = new JButton("12");
-		btn12.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn12.setBounds(305, 180, 95, 80);
-		panel.add(btn12);
-		
-		JButton btn13 = new JButton("13");
-		btn13.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn13.setBounds(20, 260, 95, 80);
-		panel.add(btn13);
-		
-		JButton btn14 = new JButton("14");
-		btn14.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn14.setBounds(115, 260, 95, 80);
-		panel.add(btn14);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(20, 350, 380, 70);
-		panel.add(panel_1);
-		panel_1.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JButton btnReinicio = new JButton("Reiniciar");
-		btnReinicio.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnReinicio.setFont(new Font("Tahoma", Font.BOLD, 16));
-		panel_1.add(btnReinicio);
-		
-		JButton btn15 = new JButton("15");
-		btn15.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btn15.setBounds(210, 260, 95, 80);
-		panel.add(btn15);
-		
-		JButton emptyButton = new JButton("");
-		emptyButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		emptyButton.setBounds(305, 260, 95, 80);
-		emptyButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		panel.add(emptyButton);
-	}
-
-}
+   
+    public void reiniciar()
+    {
+    	numbers.clear();
+        for (int i = 1; i <= 16; i++) 
+        {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        
+        revalidate();
+        repaint();
+    }
+    
+    
+} //nup
